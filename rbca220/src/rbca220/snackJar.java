b66bb8a79f0f8ca8f3b6f7b5a2524595c9a593d8
bca <bca@Bcalab4-m09>
2023-02-25 10:28:06 +0530

    i have pushed the sack jar project

---
+++ rbca220/src/rbca220/snackJar.java
@@ -0,0 +1,16 @@
+package rbca220;
+
+public class snackJar {
+	int num_biscuits;
+	  void SnackJar(int num_biscuits)
+		{
+			this.num_biscuits = num_biscuits;
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		snackJar sj = new snackJar();
+		
+	}
+
+}
